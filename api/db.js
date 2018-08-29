614991ed47b5d6f7275b2b995fba1de14d121dd7
alex <rboots@ya.ru>
2018-08-30 02:10:44 +0300

    psql_docker

--- api/db.js
+++ api/db.js
@@ -3,10 +3,7 @@ const path = require('path');
 const config = require('config').db;
 const Sequelize = require('sequelize');
 
-const db = new Sequelize({
-  dialect: 'sqlite',
-  storage: path.resolve(__dirname, `../../${config.name}.sqlite`),
-});
+const db = new Sequelize(config.database, config.username, config.password, config.config);
 
 require('./models/user').init(db);
 require('./models/report').init(db);
