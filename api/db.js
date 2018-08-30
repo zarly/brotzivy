9333956fe1d6a019eb88865b97e8aba8bf83d7f3
alex <rboots@ya.ru>
2018-08-30 04:12:40 +0300

    secret

--- api/db.js
+++ api/db.js
@@ -3,7 +3,7 @@ const path = require('path');
 const config = require('config').db;
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(config.database, config.username, config.password, config.config);
+const db = new Sequelize(config.database, config.username, process.env.PG_PASSWORD, config.config);
 
 require('./models/user').init(db);
 require('./models/report').init(db);
